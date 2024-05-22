a8495d8541a3da8dc7d9c963a785cd67b79ca231
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-23 00:36:46 +0600

    Update C.java

--- src/C.java
+++ src/C.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class C {
+public class C {
+      System.out.println("Список C: "+C);
+
+    // g) Сортировка списка C по длине строк и отображение отсортированного списка
+        Collections.sort(C,Comparator.comparingInt(String::length));
+        System.out.println("Отсортированный список C: "+C);
+      }
 }

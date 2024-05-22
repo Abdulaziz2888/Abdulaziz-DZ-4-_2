bc221a99eaa6e9077ece19e6e55b35a5429d001c
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-23 00:37:03 +0600

    Update A.java

--- src/A.java
+++ src/A.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class A {
+public class A {
+    System.out.println("Список A: " + A);
+
+    // d) Считывание еще 5 строк и сохранение их в список B
+        System.out.println("Введите 5 строк для списка B:");
+        for (int i = 0; i < 5; i++) {
+        System.out.print("Строка " + (i + 1) + ": ");
+        B.add(scanner.nextLine());
+    }
 }

7a7483432ba438fb70ab88c20268367e125f9031
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-23 00:36:20 +0600

    Update B.java

--- src/B.java
+++ src/B.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class B {
+public class B {
+     System.out.println("Список B: " + B);
+
+    // e) Объединение списков A и B в новый список C в указанном порядке
+    List<String> C = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+        C.add(A.get(i));
+        C.add(B.get(4 - i));
+    }
 }
